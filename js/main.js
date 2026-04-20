32e211dd0e03b4914133fec8178504406b327c35
JusaTiVi <jusa.itkonen@esedulainen.fi>
2026-04-20 13:09:23 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 function testaa() {
-    
+    let ID = document.getElementById("kayttaja").value;
+    let salasana = document.getElementById("salasana").value;
+    let nimi = document.getElementById("nimi").value;
+    let osoite = document.getElementById("osoite").value;
+    let maa = document.getElementById("maa").value;
 }
